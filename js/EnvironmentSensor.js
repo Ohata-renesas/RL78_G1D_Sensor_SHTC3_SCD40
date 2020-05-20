55ac3968f128aa2d253e9ebcc771785fd34baf15
Ohata-renesas <tomoyuki.ohata.uh@renesas.com>
2020-05-20 16:16:47 +0900

    17th commit

--- js/EnvironmentSensor.js
+++ js/EnvironmentSensor.js
@@ -27,7 +27,7 @@
       .then(device => {
         document.getElementById('statusText').innerHTML = "Connect the device"
         this.device = device;
-        this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
+        this.device.addEventListener('gattserverdisconnected', this.disconnect);
         return device.gatt.connect();
       })
       .then(server => {
