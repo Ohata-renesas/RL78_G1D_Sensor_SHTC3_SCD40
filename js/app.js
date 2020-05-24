e7dbafe287c65a750947922e26f46ad5c58edf8f
Ohata-renesas <tomoyuki.ohata.uh@renesas.com>
2020-05-24 17:37:11 +0900

    25th commit

--- js/app.js
+++ js/app.js
@@ -341,7 +341,7 @@ function changeTextFontSize() {
 function changeFontSize(element) {
   // element = calculateWidthAndHeight(element);
   // element.height = parseInt(getComputedStyle(element).height.slice(0, -2)) / devicePixelRatio;
-  console.log(element);
+  console.log(element.style);
   element.style.fontSize = parseInt(getComputedStyle(element).height.slice(0, -2)) / devicePixelRatio + "px";
   console.log(element.style.fontSize);
 }
