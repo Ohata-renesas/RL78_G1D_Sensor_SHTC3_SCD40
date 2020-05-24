ca6cef081e0657d5a83bb737bef38addcc1274ce
Ohata-renesas <tomoyuki.ohata.uh@renesas.com>
2020-05-24 19:35:33 +0900

    50th commit

--- js/app.js
+++ js/app.js
@@ -79,8 +79,7 @@ function drawBgMeterGraph(id, context, x0, y0, radius) {
 // Draw background circle
 function drawBgCircle(context, x0, y0, radius) {
   context.lineWidth   = radius / 30;
-  // context.strokeStyle = renesasGray;
-  context.strokeStyle = sensirionGreen;
+  context.strokeStyle = renesasGray;
   drawCanvas.drawCircle(context, x0, y0, radius);
 }
 
@@ -94,24 +93,32 @@ function drawBgText(id, context, x0, y0, radius) {
   let yCoordinateOfUnit  = y0 + radius / 3 + parseInt(fontSizeOfUnit)  / 2;
 
   context.textAlign = "center";
-  context.fillStyle = renesasGray;
 
   switch(id) {
     case sensorInfo.temperature.sensorID :
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.temperature.text.name,  x0, yCoordinateOfName,  fontSizeOfName);
+      context.fillStyle = renesasBlue;
       drawCanvas.drawText(context, sensorInfo.temperature.text.value, x0, yCoordinateOfValue, fontSizeOfValue);
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.temperature.text.unit,  x0, yCoordinateOfUnit,  fontSizeOfUnit);
     break;
 
     case sensorInfo.humidity.sensorID :
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.humidity.text.name,  x0, yCoordinateOfName,  fontSizeOfName);
+      context.fillStyle = renesasBlue;
       drawCanvas.drawText(context, sensorInfo.humidity.text.value, x0, yCoordinateOfValue, fontSizeOfValue);
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.humidity.text.unit,  x0, yCoordinateOfUnit,  fontSizeOfUnit);
     break;
 
     case sensorInfo.co2.sensorID :
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.co2.text.name,  x0, yCoordinateOfName,  fontSizeOfName);
+      context.fillStyle = renesasBlue;
       drawCanvas.drawText(context, sensorInfo.co2.text.value, x0, yCoordinateOfValue, fontSizeOfValue);
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.co2.text.unit,  x0, yCoordinateOfUnit,  fontSizeOfUnit);
     break;
 
@@ -163,7 +170,7 @@ function drawFgMeterGraph(id, context, x0, y0, radius) {
   let rangeOfAngle    = (currentEndAngle[id] - oldEndAngle[id]) / maxNumberOfRepetition;
   radius             *= 0.85;
   context.lineWidth   = radius / 10;
-  context.strokeStyle = renesasBlue;
+  context.strokeStyle = sensirionGreen;
   drawCanvas.drawArc(context, x0, y0, radius, startAngle, oldEndAngle[id] + rangeOfAngle * (countOfRepetition + 1));
 }
 
