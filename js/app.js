e2d570fe0412c96ee5f2476e753b59cfa4d2caad
Ohata-renesas <tomoyuki.ohata.uh@renesas.com>
2020-05-24 19:47:23 +0900

    50th commit

--- js/app.js
+++ js/app.js
@@ -98,7 +98,7 @@ function drawBgText(id, context, x0, y0, radius) {
     case sensorInfo.temperature.sensorID :
       context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.temperature.text.name,  x0, yCoordinateOfName,  fontSizeOfName);
-      context.fillStyle = sensirionGreen;
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.temperature.text.value, x0, yCoordinateOfValue, fontSizeOfValue);
       context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.temperature.text.unit,  x0, yCoordinateOfUnit,  fontSizeOfUnit);
@@ -107,7 +107,7 @@ function drawBgText(id, context, x0, y0, radius) {
     case sensorInfo.humidity.sensorID :
       context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.humidity.text.name,  x0, yCoordinateOfName,  fontSizeOfName);
-      context.fillStyle = sensirionGreen;
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.humidity.text.value, x0, yCoordinateOfValue, fontSizeOfValue);
       context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.humidity.text.unit,  x0, yCoordinateOfUnit,  fontSizeOfUnit);
@@ -116,7 +116,7 @@ function drawBgText(id, context, x0, y0, radius) {
     case sensorInfo.co2.sensorID :
       context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.co2.text.name,  x0, yCoordinateOfName,  fontSizeOfName);
-      context.fillStyle = sensirionGreen;
+      context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.co2.text.value, x0, yCoordinateOfValue, fontSizeOfValue);
       context.fillStyle = renesasGray;
       drawCanvas.drawText(context, sensorInfo.co2.text.unit,  x0, yCoordinateOfUnit,  fontSizeOfUnit);
